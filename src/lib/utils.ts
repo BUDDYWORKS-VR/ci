57b66bcd005990b8fec7df8b2a1fd1ac726ede3c
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2025-03-13 19:34:16 +0100

    And another one

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,63 +1,8 @@
-import { type CollectionEntry, getCollection } from "astro:content";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function formatDate(date: Date) {
-  return date.toISOString().split("T")[0];
-}
-
-export function readingTime(html: string) {
-  const textOnly = html.replace(/<[^>]+>/g, "");
-  const wordCount = textOnly.split(/\s+/).length;
-  const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
-  return `${readingTimeMinutes} min read`;
-}
-
-export function sortByLastUpdateDate<T extends CollectionName>(
-  a: CollectionEntry<T>,
-  b: CollectionEntry<T>,
-) {
-  return (
-    (b.data.lastUpdateDate ?? b.data.date).getTime() -
-    (a.data.lastUpdateDate ?? a.data.date).getTime()
-  );
-}
-
-export async function getFilteredCollectionEntries<T extends CollectionName>(
-  collectionName: T
-): Promise<{
-  entries: CollectionEntry<T>[];
-}> {
-  const data = (await getCollection(collectionName))
-    .filter((post: CollectionEntry<T>) => !post.data.draft)
-    .sort(
-      sortByLastUpdateDate
-    );
-
-  return { entries: data };
-}
-
-export async function getNavigationEntries<T extends CollectionName>(
-  collectionName: T,
-  referenceSlug: string | undefined,
-): Promise<{ nextPost?: CollectionEntry<T>; prevPost?: CollectionEntry<T> }> {
-  if (!referenceSlug) {
-    return {};
-  }
-
-  const { entries } = await getFilteredCollectionEntries(collectionName);
-  const currentIndex = entries.findIndex(
-    (entry) => entry.slug === referenceSlug,
-  );
-
-  return {
-    nextPost: entries[currentIndex + 1],
-    prevPost: entries[currentIndex - 1],
-  };
 }
 
 export function resolvePath(href: string | undefined | null, currentPath?: string | undefined) {
@@ -82,28 +27,4 @@ export function resolvePath(href: string | undefined | null, currentPath?: strin
     : (currentPath ?? "").replace(/\/$/, "") + "/" + href;
 
   return resolvedPath;
-}
-
-export function formatDateWithLastUpdateDate(date: Date, lastUpdateDate?: Date): string {
-  const formattedDate = date.toISOString().substring(0, 10);
-
-  if (lastUpdateDate) {
-    const formattedLastUpdateDate = lastUpdateDate.toISOString().substring(0, 10);
-    return `${formattedDate} (updated: ${formattedLastUpdateDate})`;
-  }
-  return formattedDate;
-}
-
-export async function getAllEntriesWithTags() {
-  const entries = [
-    ...(await getFilteredCollectionEntries("blog")).entries,
-    ...(await getFilteredCollectionEntries("talks")).entries,
-    ...(await getFilteredCollectionEntries("projects")).entries,
-  ].sort(sortByLastUpdateDate);
-
-  const tags = [
-    ...new Set(entries.flatMap((entry) => entry.data.tags || [])),
-  ].sort();
-
-  return { tags, entries }
 }

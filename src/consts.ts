59931efb282a5532889ad537c742222cc90c1f29
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2025-03-13 19:10:44 +0100

    Initial Commit

---
+++ src/consts.ts
@@ -0,0 +1,52 @@
+
+
+
+export type GlobalSite = {
+  title: string;
+  description: string;
+  author: string;
+  authorPhotoSrc: string;
+  logo?: {
+    darkThemeSrc?: string;
+    lightThemeSrc?: string;
+  };
+};
+
+export const GLOBAL: GlobalSite = {
+  title: "BUDDYWORKS",
+  description: "Official BUDDYWORKS design guidelines.",
+  author: "BUDDYWORKS",
+  logo: {
+    darkThemeSrc: "/logo/logo_dark.png",
+    lightThemeSrc: "/logo/logo_light.png",
+  }
+};
+
+type ContactInfo = {
+  type: string;
+  href: string;
+  displayAs?: string;
+}
+
+type ContactSite = ContactInfo[]
+
+export const CONTACT: ContactSite = [
+  {
+    type: "Email",
+    href: "mailto:email@example.com",
+    displayAs: "email@example.com",
+  },
+  {
+    type: "X",
+    href: "https://x.com/BillGates",
+    displayAs: "@BillGates on X",
+  },
+  {
+    type: "GitHub",
+    href: "https://github.com/dotnet",
+  },
+  {
+    type: "LinkedIn",
+    href: "https://www.linkedin.com/in/williamhgates/",
+  },
+];

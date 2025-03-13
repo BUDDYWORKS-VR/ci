d39ba90165f8d2ab546221093247e0e2cec28dfd
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2025-03-13 20:34:47 +0100

    Fixed an oopsie.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ci.buddyworks.wtf",
-  integrations: [tailwind(), sitemap(), mdx()],
+  integrations: [tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
